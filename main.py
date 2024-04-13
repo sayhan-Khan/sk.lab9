4296f6e1532184cabd8ee593d01b029e9242221b
sayhan-Khan <khan.sayhan@ufl.edu>
2024-04-13 16:19:26 -0400

    Sayhan Update Main
    Used parthners main function to ensure compatability

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ def encode(password):
         encoded_digit = str((int(digit) + 3) % 10)
         encoded_password += encoded_digit
     return encoded_password
-
-
-if __name__ == "__main__":
+def main():
     while True:
-        break
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode_password(password)
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            if 'encoded_password' in locals():
+                original_password = decode_password(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            else:
+                print("No password has been encoded yet!")
+        elif option == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Please try again.")
+if __name__ == "__main__":
+    main()
