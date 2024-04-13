99449384333a30a634ef1484aa4e93d8b284ae90
sayhan-Khan <khan.sayhan@ufl.edu>
2024-04-13 16:44:38 -0400

    final changes
    updated encode name, changed test output to given sample output.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def encode(password):
 def decode_password(encoded_password):
     decoded_password = ""
     for digit in encoded_password:
-        original_digit = str((int(digit) - 3) % 10)  # Shifting each digit down by 3
+        original_digit = str((int(digit) - 3) % 10)
         decoded_password += original_digit
     return decoded_password
 
@@ -19,22 +19,23 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
+        print("-------------")
         option = input("Please enter an option: ")
 
         if option == "1":
             password = input("Please enter your password to encode: ")
-            encoded_password = encode_password(password)
-            print("Your password has been encoded and stored!")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
         elif option == "2":
             if 'encoded_password' in locals():
                 original_password = decode_password(encoded_password)
-                print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+                print(f"The encoded password is {encoded_password}, and the original password is {original_password}.\n")
             else:
                 print("No password has been encoded yet!")
         elif option == "3":
-            print("Goodbye!")
             break
         else:
             print("Invalid option. Please try again.")
+
 if __name__ == "__main__":
     main()
